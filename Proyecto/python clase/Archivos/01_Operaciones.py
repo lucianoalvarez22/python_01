0f27d4339a06f438ddebbbb3f6a29ac0dac2d35c
lucianoalvarez22 <xianoalvarez@gmail.com>
2022-11-18 19:55:00 +0100

    nuevas actualizaciones

--- Proyecto/python clase/Archivos/01_Operaciones.py
+++ Proyecto/python clase/Archivos/01_Operaciones.py
@@ -1,6 +1,11 @@
 #OPERACIONES CON ARCHIVOS
-#Modos de archivos "r" (read) "w" (writer) "a" (append para abrir), "w+" (lectura y escritura, crea archivos inexistentes) "r+" (leer y escritura)
-# "a+" (Para )
+#Modos de archivos "r" (read) 
+# "w" (writer) 
+# "a" (append para abrir y añadir) 
+# "w+" (lectura y escritura, crea archivos inexistentes. Si no existe, lo crea. Y si existe, borra el existente y se sustituye por el nuevo) 
+# "r+" (leer y escritura. Si no existe da un error)
+# "a+" (Para lectura y escritura. Si existe, se añade al final)
+# "b" (abre el archivo en modo binario)
 
 archivo = open('/home/luciano/git1/python_01/Proyecto/python clase/Archivos/pruebas.txt','r')
 numeros = []
---
+++ Proyecto/python clase/Archivos/02_hurricanes.py
@@ -0,0 +1,31 @@
+def limpiar (fila): #Funcion para limpiar filas de comillas, espacios en blanco y saltos de linea
+    salida = [] #Lista vacias donde meteremos el resultado de la funcion
+    for elem in fila:
+        elem = elem.replace('"', '').strip()
+        elem = elem.replace('\n', '')
+        salida.append(elem)
+    
+    return salida
+
+
+
+ruta_base = '/home/luciano/git1/python_01/Proyecto/python clase/Archivos/'
+datos = ruta_base + 'hurricanes.csv'
+
+huracanes = []
+
+archivo = open(datos,'r')
+cabeceras = archivo.readline()
+claves = limpiar(cabeceras.split(","))
+
+for linea in archivo:
+    fila = limpiar(linea.split(","))
+    d = {}
+    for i in range (len(fila)):
+        d[claves[i]] = fila [i]
+    huracanes.append(d)
+
+import pprint
+
+pprint.pprint(huracanes)
+
