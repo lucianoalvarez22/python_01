fc33be6ca506f03e3259d56ed873e863f46bc0f7
lucianoalvarez22 <xianoalvarez@gmail.com>
2022-11-18 12:19:56 +0100

    ejercicio

--- Proyecto/python clase/Codewars/EjercicioCasa02_Codewars.py
+++ Proyecto/python clase/Codewars/EjercicioCasa02_Codewars.py
@@ -4,7 +4,7 @@
 
 #Los números romanos modernos se escriben expresando cada dígito por separado, comenzando con el dígito más a la izquierda y omitiendo cualquier dígito con un #valor de cero. En números romanos, 1990 se representa: 1000=M, 900=CM, 90=XC; resultando en MCMXC. 2008 se escribe como 2000=MM, 8=VIII; o MMVIII. 1666 utiliza #cada símbolo romano en orden descendente: MDCLXVI.
 
-def solution(numero_romanos):
+""" def solution(numero_romanos):
 
     simbolo = {
         1:"I",
@@ -27,3 +27,20 @@ def solution(numero_romanos):
 
 numero = int(input("Introduce un numero: "))
 print(solution(numero))
+ """
+
+integer_roman = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+
+def integer_to_roman(integer):
+    roman = '' # Se declara la variable roman y se le asigna una cadena vacía.
+
+    while integer > 0: # Mientras el número ingresado sea mayor a cero.
+        for i, r in integer_roman: # Iteramos en la lista integer_roman sobre cada par entero-romano.
+            while integer >= i: # Mientras el número sea mayor o igual al iterable i. 95 >= 90 -> roman = XC -> integer = 5
+                roman += r # A la variable roman le sumamos lo que vale r.
+                integer -= i # A la variable num le sumamos lo que vale r.
+
+    return roman # Regresamos el número romano.
+
+integer = int(input('Ingrese un número entero: '))
+print(f'El número ingresado es: {integer_to_roman(integer)}')
