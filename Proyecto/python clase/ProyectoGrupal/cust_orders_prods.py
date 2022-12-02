7f8bd5c711e6173de81c134dc7556318ad3d925c
lucianoalvarez22 <xianoalvarez@gmail.com>
2022-12-02 19:51:21 +0100

    actualizacion

--- Proyecto/python clase/ProyectoGrupal/cust_orders_prods.py
+++ Proyecto/python clase/ProyectoGrupal/cust_orders_prods.py
@@ -1,10 +1,12 @@
 import csv
 import os
 
-ruta_base = '/home/luciano/git1/python_01/Proyecto/python clase/ProyectoGrupal/'
+ruta_base = '/home/sfenrris/Desktop/ProyectoGrupal/'
+
 datos = ruta_base + 'cust_orders_prods.csv'
 
 def menu():
+    print('MENU')
     print('1.- Porcentaje de ventas de cada vendedor.')
     print('2.- Porcentaje de ventas de cada cliente.')
     print('3.- Cinco productos mas vendidos.')
@@ -13,7 +15,6 @@ def menu():
 
 def limpiapantalla():
     os.system('clear')
-
 
 def leer(archivo):
     f = open(archivo,'r')
@@ -26,7 +27,8 @@ def dame_ventas_totales(archivo_devuelto):
     contador_quantity_total = 0
     for v in archivo_devuelto:
         quantity = int(v['quantity'])
-        contador_quantity_total += quantity
+        precio = int(v['unit_price'])
+        contador_quantity_total += (quantity * precio)
     return contador_quantity_total
 
 def dame_nombres_empleados(archivo_devuelto):
@@ -43,33 +45,116 @@ def dame_venta_empleados(archivo_devuelto,nombre_empleados):
     for nombre in nombre_empleados:
         for v in archivo_devuelto:
             if v['employee_name'] == nombre:
-                contador_venta_empleado += int((v['quantity']))
+                contador_venta_empleado += (int(v['quantity']) * int(v['unit_price']))
         diccionario_vacio = {}
         diccionario_vacio[nombre] = contador_venta_empleado
         lista_ventas_empleados.append(diccionario_vacio)
-        contador_venta_empleado == 0
+        contador_venta_empleado = 0
     return lista_ventas_empleados
 
+def porcentaje_empleados(lista_venta_empleados, ventas_totales, archivo_devuelto):
+    for i in lista_venta_empleados:
+        for k, v in i.items():
+            porcentaje_vendedor = round((( v / ventas_totales) * 100) ,2)
+            print(f'{k}: {porcentaje_vendedor}%')
+    return        
 
+def dame_nombre_clientes(archivo_devuelto):
+    nombre_clientes = []
+    for e in archivo_devuelto:
+        nombre_clientes_repetidos = e['customer_name']
+        if nombre_clientes_repetidos not in nombre_clientes:
+            nombre_clientes.append(nombre_clientes_repetidos)
+    return nombre_clientes
 
+def dame_compra_clientes(archivo_devuelto, nombre_clientes):
+    lista_compra_clientes = []
+    contador_compra_cliente = 0
+    for nombre in nombre_clientes:
+        for v in archivo_devuelto:
+            if v['customer_name'] == nombre:
+                contador_compra_cliente += (int(v['quantity']) * int(v['unit_price']))
+        diccionario_vacio = {}
+        diccionario_vacio[nombre] = contador_compra_cliente
+        lista_compra_clientes.append(diccionario_vacio)
+        contador_compra_cliente = 0
+    return lista_compra_clientes
 
-limpiapantalla()
+def porcentaje_clientes(lista_compra_clientes, ventas_totales, archivo_devuelto):
+    for i in lista_compra_clientes:
+        for k, v in i.items():
+            porcentaje_clientes = round((( v / ventas_totales) * 100) ,2)
+            print(f'{k}: {porcentaje_clientes}%')
+    return  
+
+def dame_nombre_producto(archivo_devuelto):
+    nombre_producto = []
+    for p in archivo_devuelto:
+        nombre_productos_repetidos = p['product_name']
+        if nombre_productos_repetidos not in nombre_producto:
+            nombre_producto.append(nombre_productos_repetidos)
+    return nombre_producto
+
+def productos_vendidos(dame_nombre_producto, archivo_devuelto):
+    lista_venta_productos = []
+    contador_venta_productos = 0
+    for nombre in dame_nombre_producto:
+        for v in archivo_devuelto:
+            if v['product_name'] == nombre:
+                contador_venta_productos +=  int(v['quantity'])
+        diccionario_vacio = {}
+        diccionario_vacio[nombre] = contador_venta_productos
+        lista_venta_productos.append(diccionario_vacio)
+        contador_venta_productos = 0
+    return lista_venta_productos
+
+def los_mas_vendidos(lista_productos_vendidos, archivo_devuelto):
+    lista_cant_prod = []
+    for i in lista_productos_vendidos:
+        for k, v in i.items():
+            lista_cant_prod.append(v)
+    max(lista_cant_prod)
+    print(lista_cant_prod)
+            
+    return  
+
 archivo_devuelto = leer(datos)
 nombre_empleados = dame_nombres_empleados(archivo_devuelto)
-dame_ventas_totales(archivo_devuelto)
+lista_venta_empleados = dame_venta_empleados(archivo_devuelto, nombre_empleados)
+nombre_clientes = dame_nombre_clientes(archivo_devuelto)
+lista_compra_clientes = dame_compra_clientes(archivo_devuelto, nombre_clientes)
+ventas_totales = dame_ventas_totales(archivo_devuelto)
+dame_nombre_producto = dame_nombre_producto(archivo_devuelto)
+lista_productos_vendidos = productos_vendidos(dame_nombre_producto, archivo_devuelto)
+
+'''print(dame_ventas_totales(archivo_devuelto))
 dame_nombres_empleados(archivo_devuelto)
 print(dame_venta_empleados(archivo_devuelto,nombre_empleados))
+print(porcentaje_empleados(lista_venta_empleados, ventas_totales, archivo_devuelto)) 
+limpiapantalla()'''
 
 
 
 
+def main():
+    limpiapantalla()
 
-# def main():
-#     limpiapantalla()
-#     menu()
+    while True:
+        menu()
+        opcion = input('Elije una opcion: ')
+        limpiapantalla()
 
-#     opcion = input('Elije una opcion: ')
+        if opcion == '1':
+            limpiapantalla()
+            print(porcentaje_empleados(lista_venta_empleados, ventas_totales, archivo_devuelto))
+            
+        if opcion == '2':
+            limpiapantalla()
+            print(porcentaje_clientes(lista_compra_clientes, ventas_totales, archivo_devuelto))
+            
+        if opcion == '5':
+            break
+    print('Programa finalizado')    
 
-#     if opcion == 1:
-#         vendedor()
 
+main()
